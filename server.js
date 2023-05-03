d524cf940204c830e3b20bda45d647bb8c2605d3
Sivakumaran E <sivakumaran.e@gmail.com>
2023-05-03 19:04:29 +0530

    edit, delete, slugify and rest

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Article = require("./models/article");
 const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
 const app = express();
 
 mongoose.connect("mongodb://localhost/blog");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test article",
-      createdAt: new Date(),
-      description: "Test description",
-    },
-    {
-      title: "Test article 2",
-      createdAt: new Date(),
-      description: "Test description 2",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
   res.render("articles/index", { articles: articles });
 });
 
